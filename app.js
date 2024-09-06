a04c55666660235add8d5190149d288978dbdaa5
abdulsamadshah <sabdulsamad272@gmail.com>
2024-09-06 09:18:25 +0530

    quiz functionality completed

--- app.js
+++ app.js
@@ -1,18 +1,25 @@
 const express = require("express");
 require("dotenv").config({ path: `${process.cwd()}/.env` });
+const path = require('path'); // Import the path module
 const CategoryRoutes = require("./Routes/CategoryRouters");
 const QuizRouters = require("./Routes/QuizRouters");
-
 
 const globalErrorHandler = require("./controller/errorController");
 const AppError = require("./utils/appError");
 const app = express();
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use("/api/v1/", CategoryRoutes,QuizRouters);
+// Serve static files from the 'uploads/categories' directory
+app.use("/uploads/categories", express.static(path.join(__dirname, 'uploads', 'categories')));
+app.use("/uploads/products", express.static(path.join(__dirname, 'uploads', 'products')));
 
 
+// Routes
+app.use("/api/v1/", CategoryRoutes, QuizRouters);
+
+// Handle undefined routes
 app.all('*', (req, res, next) => {
     const err = new AppError(`Can't find ${req.originalUrl} on the server!`, 404);
     next(err);
